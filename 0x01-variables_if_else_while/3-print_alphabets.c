7b3c5ad76dd34e7242bdcdcc67e1fe3c0dad07b9
Nathanaelfowler <natafowler1@gmail.com>
2022-09-10 04:37:19 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,10 +13,10 @@ int main(void)
 
 	putchar('\n');
 
-	char ch;
+	char c;
 
-	for (ch = 'A'; ch <= 'Z'; ch++)
-		putchar(ch);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
 
 	putchar('\n');
 	return (0);
