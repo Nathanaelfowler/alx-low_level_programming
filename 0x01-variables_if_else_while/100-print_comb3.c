5a7186908ef0c625fad583a7591f7bb2f6310eb0
Nathanaelfowler <natafowler1@gmail.com>
2022-09-11 11:33:46 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,10 +11,13 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = i + 1; j <= '9'; j++)
 		{
-			putchar(i);
-			putchar(j);
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+			}
 
 			if (i == 9)
 			{
