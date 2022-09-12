26809dfe0797b3e246869628de6288e4df2ffb81
Nathanaelfowler <natafowler1@gmail.com>
2022-09-11 22:33:10 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,32 +6,28 @@
  */
 int main(void)
 {
-	int i, j, k, l;
+	int i, j;
 
 	for (i = '0'; i <= '9'; i++)
 	{
 		for (j = i + 1; j <= '9'; j++)
 		{
-			for (k = j + 1; k <= '9'; k++)
-			{
-				for (l = k + 1; l <= '9'; l++)
+			if (i != i)
+			{	putchar(' ');
+				if (j != j)
 				{
-					if (i && j != k && l)
-					{ 
-						putchar(i);
-						putchar(j);
-						putchar(k);
-						putchar(l);
-						putchar(' ');
+					putchar(i);
+					putchar(i);
+					putchar(j);
+					putchar(j);
 
-						if (i == '8'; j == '8' && k == '9'; l == '9')
-							continue;
-						putchar(',');
-						putchar(' ');
-					}
+					if (i == '98' && j == '99')
+						continue;
+					putchar(',');
+					putchar(' ');
 				}
 			}
-		}
+		}			
 	}
 	putchar('\n');
 	return (0);
