ac4bc49c4a573b54ea254571a803ae2d439be090
Nathanaelfowler <natafowler1@gmail.com>
2022-09-13 13:02:58 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - entry point
+ * @c : an int
+ * Return: 0
+ */
+int print_last_digit(int c)
+{
+	int d;
+	d= c % 10
+
+	_putchar('c');
+	_putchar('d');
+
+	return ('d');
+}
