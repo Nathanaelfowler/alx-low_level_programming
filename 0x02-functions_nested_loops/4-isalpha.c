130177eb4e35421a3add0e059bfeb891c1f2adaf
Nathanaelfowler <natafowler1@gmail.com>
2022-09-13 10:28:13 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,14 +10,13 @@ int _isalpha(int c)
 	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
-
-		else if (c >= 'A' && c <= 'Z')
-		{
-			return (1);
-		}
-		{
-			else
-				return (0);
-		}
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 }
