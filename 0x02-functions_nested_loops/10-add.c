c32990aa56e887cabd80f9951d520b22ca71e201
Nathanaelfowler <natafowler1@gmail.com>
2022-09-13 15:44:58 -0700

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,13 +4,10 @@
  * add - entry point
  * @c: an int
  * @n: an int
- * @s: an int
  * Return: 0
  */
-int add(int c, int n, int s)
+int add(int c, int n)
 {
-	s = c +n;
-
 	_putchar(c + n);
 	return (c + n);
 }
