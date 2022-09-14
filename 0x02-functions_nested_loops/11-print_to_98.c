e5bf1743c75b499a4d608453ba7be4f96e7c1416
Nathanaelfowler <natafowler1@gmail.com>
2022-09-14 03:22:04 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,33 +8,13 @@
  */
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n <= 98)
 	{
-		while ( n <= 98)
-		{
-			_putchar('n');
-			if (n != 98)
-			{
-				_putchar(",");
-			}
-			n++;
-		}
-	}
-	else if (n > 98)
-	{
-		while (n >= 98)
-		{
-			_putchar('n');
-			if (n != 98)
-			{
-				_putchar(",");
-			}
-			n--;
-		}
+		for (n = n; n <= 97; n++)
+			printf("%d, ", n);
 	}
 	else
-	{
-		_putchar(98);
-	}
-	_putchar('\n');
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+	printf("98\n");
 }
