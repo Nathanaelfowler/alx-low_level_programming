8f4f8d9bc5e4fdc20ee0de0d54d0bcf865689660
Nathanaelfowler <natafowler1@gmail.com>
2022-09-16 02:36:26 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - entry point
+ * @a: an int
+ * @b: an int
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int t;
+	t = a * b;
+
+	_putchar('t');
+	return (0);	
+}
