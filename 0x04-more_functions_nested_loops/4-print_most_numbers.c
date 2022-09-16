cab8a23693e91b1529503ed0be14e157679a83d0
Nathanaelfowler <natafowler1@gmail.com>
2022-09-16 13:51:23 -0700

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_most_numbers - entry point
- * Return: 0
+ * Return: returns nothing
  */
 void print_most_numbers(void)
 {
@@ -10,10 +10,8 @@ void print_most_numbers(void)
 
 	for (c = 0; c <= 9; c++)
 	{
-		if ((c == 2) || (c == 4))
-			continue;
-		else
-			_putchar(c + '0');
+		if (c != 2 && c != 4)
+			_putchar(c);
 	}
 	_putchar('\n');
 }
