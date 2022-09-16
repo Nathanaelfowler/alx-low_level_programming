0dd08c91bbf6802a2789faa4bd96ebd0344e4b5a
Nathanaelfowler <natafowler1@gmail.com>
2022-09-16 14:42:51 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,10 +8,15 @@ void more_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 14; i++)
+	while (i <= 9)
 	{
-		_putchar((i % 10) + '0');
+
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+
+		i++
 	}
-	_putchar('\n');
-	_putchar(x 10);
 }
