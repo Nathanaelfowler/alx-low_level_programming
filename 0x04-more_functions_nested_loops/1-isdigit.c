20cd2b25b5b52e098ad533807149da72a37dcf3e
Nathanaelfowler <natafowler1@gmail.com>
2022-09-16 02:12:07 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - entry point
+ * @c: an int
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+	if (_isdigit(c))
+		return (1);
+	else
+		return (0);
+}
