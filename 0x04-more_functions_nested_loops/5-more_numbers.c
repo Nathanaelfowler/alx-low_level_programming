62d4eb650a51ba65a3ce086e751b651e57f18921
Nathanaelfowler <natafowler1@gmail.com>
2022-09-16 14:31:30 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * more_numbers - entry point
+ * Return: return numbers
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 14; i++)
+	{
+		_putchar((i % 10) + '0');
+	}
+	_putchar('\n');
+	_putchar(x 10);
+}
