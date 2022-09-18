6620d188882f54d101eb0b9db6a3a8efa8bd4d8c
Nathanaelfowler <natafowler1@gmail.com>
2022-09-18 08:55:04 -0700

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @j: an int
+ * Return: 0
+ */
+int main(int j)
+{
+	for (j = 1; j <= 100; j++)
+	{
+		if (j % 3 == 0)
+		{
+			_putchar(Fizz);
+		}
+		if (j % 5 == 0)
+		{
+			_putchar(Buzz);
+		}
+		if (j % 3 == 0 && j % 5 == 0)
+		{
+			_putchar(FizzBuzz);
+		}
+		_putchar(' ');
+	}
+}
