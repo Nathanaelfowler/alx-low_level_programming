984182f494c15cdbbe12ebcd6f85f5d69cbef668
Nathanaelfowler <natafowler1@gmail.com>
2022-09-20 13:23:39 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,16 @@
  */
 void puts2(char *str)
 {
+	int j;
 	int i = 0;
 
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
 		i++;
+	}
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
 	}
 	_putchar('\n');
 }
