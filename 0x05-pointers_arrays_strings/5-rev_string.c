8b0dbc9fb9055f4de413d1174875f9fcb53deabc
Nathanaelfowler <natafowler1@gmail.com>
2022-09-20 07:33:39 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - entry point
+ * @s: a char
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int length = strlen(s);
+	int middle = length / 2;
+	char temp;
+
+	for (int i = 0; i < middle; i++)
+	{
+		temp = s[i];
+		s[i] = s[lenght - i - 1];
+		s[length - i - 1] = temp;
+	}
+}
