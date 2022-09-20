0d78a1f1fbf84b347d0419333320de54a77bc784
Nathanaelfowler <natafowler1@gmail.com>
2022-09-20 12:05:29 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 - entry point
+ * @str: a char
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
