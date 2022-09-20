aabda10999564376563751b6dea45f2f3fae6a98
Nathanaelfowler <natafowler1@gmail.com>
2022-09-20 14:33:32 -0700

    Task 87

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - entry point
+ * @a: an int
+ * @n: an int
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", a[i]);
+	}
+	printf(",");
+	printf("\n");
+}
