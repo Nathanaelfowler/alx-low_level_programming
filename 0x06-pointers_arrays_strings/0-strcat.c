c287c22e0d8117992709b2cbcde0654baa686f32
Nathanaelfowler <natafowler1@gmail.com>
2022-09-21 14:03:06 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,9 +10,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int j;
+	int j, i;
 
-	strcat(dest, src);
-
-	dest[j] = '\0';
+	for (j = 0; dest[j]; j++)
+	{
+		for (i = 0; src[i]; i++)
+		{
+			dest[j] = src[i];
+		}
+	}
+	return (dest);
 }
