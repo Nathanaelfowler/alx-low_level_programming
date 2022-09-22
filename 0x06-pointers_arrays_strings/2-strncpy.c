177529ae65f6f6cdb591eb6290359c1f6199452e
Nathanaelfowler <natafowler1@gmail.com>
2022-09-22 04:07:58 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[n] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[n] = src[n];
+		dest[i] = src[i];
 	}
 	for (; i < n; i++)
-		dest[n] = '\0';
+		dest[i] = '\0';
 	return (dest);
 }
