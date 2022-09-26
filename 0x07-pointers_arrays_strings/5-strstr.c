210bc1b9d7294966536ee3f7453db9c23bc86c62
Nathanaelfowler <natafowler1@gmail.com>
2022-09-26 14:25:58 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,10 +5,29 @@
 /**
  * *_strstr - Write a function that locates a substring.
  * @haystack: a char
- * @neddle: a char
+ * @needle: a char
  * Return: beginning of substring or NULL
  */
 char *_strstr(char *haystack, char *needle)
 {
-	
+	int i;
+	int s = 0;
+
+	while (needle[s] != '\0')
+		s++;
+
+	while (*haystack)
+	{
+		for (i = 0; needle[i]; i++)
+		{
+			if (haystack[i] != needle[i])
+				break;
+		}
+		if (i != s)
+			haystack++;
+		else
+			return (haystack);
+
+	}
+	return (NULL);
 }
