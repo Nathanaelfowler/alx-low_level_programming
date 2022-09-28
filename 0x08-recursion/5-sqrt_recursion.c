e0e98fc6f962b17ad19d6c6822bbe9b7330f456c
Nathanaelfowler <natafowler1@gmail.com>
2022-09-28 13:10:26 -0700

    Task 6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a no.
+ * @n: an int
+ * Return: 0
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrtl(n, 1)); 
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * helper - does it for me
+ * @i: helper int
+ * @n: integer I'm testing
+ * Return: value
+ */
+int helper(int i, int n)
+{
+	if (n % i == 0 && n != i)
+		return (0);
+	if (n % i != 0 && i < n)
+		return (helper(i + 1, n));
+	return (1);
+}
+/**
+ * is_prime_number-  entry point
+ * @n: an int
+ * Return: 0
+ */
+int is_prime_number(int n)
+{
+	int i = 2;
+
+	if(n < 2)
+		return (0);
+	return (helper(i, n));
+}
