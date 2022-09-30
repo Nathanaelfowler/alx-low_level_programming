9e64de9a10f5419ba817d2377ac9ba9b5eb20b9b
Nathanaelfowler <natafowler1@gmail.com>
2022-09-30 04:07:04 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: an int
+ * @argv: a char
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc <= 1)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul = mul * atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	return (0);	
+}
