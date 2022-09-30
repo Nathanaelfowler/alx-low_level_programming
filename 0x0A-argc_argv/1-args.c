f4667229f9e765669210e8bee27ca1337fcd70ce
Nathanaelfowler <natafowler1@gmail.com>
2022-09-30 07:03:35 -0700

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,13 +7,13 @@
  * @argv: value of each string
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%d\n", i - 1);
 	}
+	printf("%d\n", i - 1);
 	return (0);
 }
