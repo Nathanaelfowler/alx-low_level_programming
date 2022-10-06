e5f48a4087dea3fde5be04e7ff659174c56e167b
Nathanaelfowler <natafowler1@gmail.com>
2022-10-06 13:10:06 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,6 +11,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -19,6 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 
-	ptr = calloc(size,  nmemb);
+	for (i = 0; i < nmemb * size; i++)
+		ptr[i] = 0;
 	return (ptr);
 }
