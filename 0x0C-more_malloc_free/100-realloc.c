57e9a72487f75d5a804c43521d0995fb49aeb1a0
Nathanaelfowler <natafowler1@gmail.com>
2022-10-07 08:32:42 -0700

    Task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_realloc - entry point
+ * @ptr: previous void pointer
+ * @old_size: unsigned int
+ * @new_size: unsigned int
+ * Return: void
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *ptr1;
+
+	if (new_size  == old_size)
+	{
+		return (ptr);
+	}
+	if (new_size > old_size)
+	{
+		ptr1 = malloc(new_size);
+		free(ptr);
+		return (ptr1);
+	}
+
+	ptr1 = realloc(ptr, new_size * sizeof(int));
+	if (ptr == NULL)
+	{
+		ptr1 = malloc(new_size);
+		return (ptr1);
+	}
+	free(ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	return (ptr);
+}
