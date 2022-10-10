1b42aa7d40d42ec885df1542f3a7b6cada34462c
Nathanaelfowler <natafowler1@gmail.com>
2022-10-10 00:59:54 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * struct dog - data type
+ * @name: the first char member
+ * @age: the second float member
+ * @owner: the third char member
+ * The name of the variable of the struct
+ * is details
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} details;
