799606ca5b22d6b1799ba6ef7e8c8dfbf1f3e287
Nathanaelfowler <natafowler1@gmail.com>
2022-10-11 04:17:33 -0700

    Task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef ABSO
-#define ABSO
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
 
-#define ABS(x) (((x) < (0)) ? ((-x)) : (x))
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
-#endif /* ABS(x) */
+#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
