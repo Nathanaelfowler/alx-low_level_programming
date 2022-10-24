689269f63ecc6e1989b0e546ff48a4652bd233fa
Nathanaelfowler <natafowler1@gmail.com>
2022-10-24 13:07:00 -0700

    Task 9

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_nodeint_at_index - unction that deletes the node at index index
+ * of a listint_t linked list.
+ * @head: a pointer of the list
+ * @index: the index of the node that should be deleted
+ * Index starts at 0
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,27 +12,32 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int count = 1, i = 1;
-	listint_t *newnode, *temp;
+	unsigned int i;
+	listint_t *temp, *newnode;
 
-	newnode = malloc(sizeof(listint_t));
-
-	if (idx < count)
-	{
+	if (head == NULL)
 		return (NULL);
-	}
-	else
+	if (idx != 0)
 	{
 		temp = *head;
-		while (i < idx)
+		for (i = 0; i < idx - 1 && temp != NULL; i++)
 		{
 			temp = temp->next;
-			i++;
 		}
-
-		newnode->n = n;
-		newnode->next = temp->next;
-		temp->next = newnode;
+		if (temp == NULL)
+			return (NULL);
 	}
-	return (temp->next);
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	if (idx == 0)
+	{
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
+	}
+	newnode->next = temp->next;
+	temp->next = newnode;
+	return (newnode);
 }
