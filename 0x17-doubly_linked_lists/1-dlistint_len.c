55e05ac58103f6cbb92e23a0028dfc64a4760560
Nathanaelfowler <natafowler1@gmail.com>
2022-12-08 06:12:54 -0800

    Task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,4 +1,19 @@
 #include "lists.h"
 
 /**
- * dlistint_len - 
+ * dlistint_len - a function that returns the number of elements
+ * in a linked dlistint_t list.
+ * @h: a dlistint_t list.
+ * Return: No. of ele
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t noele = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		noele++;
+	}
+	return (noele);
+}
