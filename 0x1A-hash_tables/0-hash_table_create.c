492e8f538a92e5e2c28a9c136524f381bf7a5fed
Nathanaelfowler <natafowler1@gmail.com>
2023-01-05 11:58:13 -0800

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,25 +5,27 @@
 
 /**
  * hash_table_create - function to create a hash table
- * size: is the size of the array
+ * @size: is the size of the array
+ * Return: a pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	/**allocate table*/
 	hash_table_t *table = malloc(sizeof(hash_table_t) * 1);
+
 	if (table == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	table->size = size;
 	/**allocate table items*/
-	table->array = malloc(sizeof(hash_table_t*) * size);
+	table->array = malloc(sizeof(hash_table_t *) * size);
 
 	for (i = 0; i < table->size; i++)
 	{
 		table->array[i] = NULL;
 	}
 
-	return(table);
+	return (table);
 }
