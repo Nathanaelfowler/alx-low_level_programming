3babe44d647c9709ca586a9f5b41adf9e2b62039
Nathanaelfowler <natafowler1@gmail.com>
2023-01-07 12:38:45 -0800

    Task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,74 @@
+#include "hash_tables.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - Adds a node
+ * @head: Array
+ * @str: String to pass
+ * @key: Key
+ * Return: Pointer to node
+ */
+hash_node_t *add_node(hash_node_t **head, const char *str, const char *key)
+{
+	hash_node_t *tmp;/*new node or item*/
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	tmp = *head;
+	tmp = malloc(sizeof(hash_node_t));
+	if (tmp == NULL)
+	{
+		return (NULL);
+	}
+	/*assigning all its contents*/
+	tmp->value = strdup(str);
+	tmp->key = strdup(key);
+	tmp->next = *head;
+	*head = tmp;
+	return (tmp);
+}
+
+/**
+ * hash_table_set - Adds an element to the hash table
+ * @ht: Hash table that i want to add or update the key/value
+ * @key: Key,can not be an empty string
+ * @value: Value associated with key
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *tmp = ht->array[idx];
+	int flag = 0;
+
+	if (ht == NULL || key == NULL || value == NULL) /* If hashtable is null*/
+		return (0);
+
+	if (tmp == NULL)/*if node or item doesn't exist*/
+	{
+		if (add_node(&(ht->array[idx]), value, key) == NULL)
+			return (0);
+	}
+	else
+	{
+		while (tmp != NULL)/*if node or item exist*/
+		{
+			if (strcmp(tmp->key, key) == 0)
+			{
+				tmp->value = strdup(value);
+				flag = 1;
+			}
+			tmp = tmp->next;
+		}
+		if (flag == 0)
+		{
+			if (add_node(&(ht->array[idx]), value, key) == NULL)
+				return (0);
+		}
+	}
+	return (1);
+}
